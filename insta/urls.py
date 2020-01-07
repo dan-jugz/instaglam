53cd0063762bde333a37a16c44dc55e71b4d695c
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-07 04:42:16 +0300

    set a route to update a post

--- insta/urls.py
+++ insta/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import PostListView,PostDetailView,PostCreateView
+from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView
 app_name = 'insta'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('new/', PostCreateView.as_view(),name='post_create'),
-    path('<int:id>/', PostDetailView.as_view(), name='post_detail'), 
+    path('<int:id>/', PostDetailView.as_view(), name='post_detail'),
+    path('<int:id>/update/', PostUpdateView.as_view(), name='post_update'), 
     path('saved/', saved_posts, name='saved_posts'),
 ]
