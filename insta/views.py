8479e7c36c43e1ea13ccf9802f1b4502e5ad376c
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-07 04:18:35 +0300

    set a detail view to show post details

--- insta/views.py
+++ insta/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from datetime import datetime
 from django.utils import timezone
 from .models import Post 
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
 class PostListView(ListView):
@@ -17,3 +17,12 @@ def saved_posts(request):
     posts = Post.objects.filter(saved=True)
     context = {'saved_posts': posts}
     return render(request, 'insta/saved_posts.html', context)
+
+
+class PostDetailView(DetailView):
+    template_name = 'insta/details.html'
+    queryset = Post.objects.all().filter(created_date__lte=timezone.now())
+
+    def get_object(self):
+        id_ = self.kwargs.get('id')
+        return get_object_or_404(Post, id=id_)
