482e99f2cf5391c897aace4168ba1caef62bc260
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-07 04:04:27 +0300

    setup the default homepage of the website to show a list of posts

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.urls import reverse 
+from datetime import datetime
+from django.utils import timezone
+from .models import Post 
+from django.views.generic import ListView
 
 # Create your views here.
+class PostListView(ListView):
+
+    template_name = 'insta/home.html'
+    queryset = Post.objects.all().filter(created_date__lte=timezone.now()).order_by('-created_date')
+    context_object_name = 'posts'
