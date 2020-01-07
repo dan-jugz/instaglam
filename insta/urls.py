b2b125f55e59fd9fda938a0b6ccd82cb21147441
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-07 04:31:02 +0300

    set the route for the detail view

--- insta/urls.py
+++ insta/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import PostListView
+from .views import PostListView,PostDetailView
 app_name = 'insta'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('<int:id>/', PostDetailView.as_view(), name='post_detail'), 
     path('saved/', saved_posts, name='saved_posts'),
 ]
