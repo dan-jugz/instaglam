b8d41db5e6f4506610e99e5a22e126d13e59e464
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-07 09:29:00 +0300

    set the static and media routes

--- instaclam/settings.py
+++ instaclam/settings.py
@@ -123,3 +123,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Directory to save our uploaded files
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
+
+LOGIN_REDIRECT_URL = 'insta:post_list'
+LOGIN_URL = 'login'
