4957ce915959c74ac441b8b5d47fc819923886cf
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-07 04:32:58 +0300

    setup a route to use  to create a post

--- insta/urls.py
+++ insta/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import PostListView,PostDetailView
+from .views import PostListView,PostDetailView,PostCreateView
 app_name = 'insta'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('new/', PostCreateView.as_view(),name='post_create'),
     path('<int:id>/', PostDetailView.as_view(), name='post_detail'), 
     path('saved/', saved_posts, name='saved_posts'),
 ]
